3b966fd0a02a6899f984b4a5f506af32797cd053
Akhila jayanthi <akhilajsharma@gmail.com>
2019-09-03 11:26:16 +0530

    initial commit

---
+++ DemoWebApp/src/main/resources/com/capg/demo/ServletLogin.java
@@ -0,0 +1,42 @@
+package com.capg.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		response.getWriter().write("servlet page");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ DemoWebApp/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+<title>Login Page</title>
+
+</head>
+<body>
+<form action="ServletLogin">
+	<h2>Login</h2>
+	<div>
+	Username:<input type="text" name="username" placeholder="username">
+	Password: <input type="password" name="password" placeholder="password">
+	<input type="button" value="submit">
+	</div>
+	</form>
+</body>
+</html>
